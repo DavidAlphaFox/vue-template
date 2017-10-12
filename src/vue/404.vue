d7f4f316846087969eacaeaea09d14ccf43a7d57
David.Gao <david.alpha.fox@gmail.com>
2017-10-12 15:13:44 +0800

    init my template of vue

---
+++ src/vue/404.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h1>
+      <router-link to="/" v-text="message"></router-link>
+    </h1>
+  </div>
+
+</template>
+
+<script lang="coffee">
+    module.exports =
+        data: ->
+            message: "Page not found"
+</script>
+
+<style lang="sass" scoped>
+    h1
+        font-family: "Arial", sans-serif
+        text-align: center
+        line-height: 100vh
+        color: #FFF
+        text-shadow: 1px 1px 10px #000
+        margin: 0
+        padding: 0
+
+    a
+        text-decoration: none
+        color: inherit
+</style>
---
+++ src/vue/Home.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h1>
+      <router-link to="/contact" v-text="message"></router-link>
+    </h1>
+  </div>
+</template>
+
+<script lang="coffee">
+    module.exports =
+        data: ->
+            message: "This is the Home page"
+</script>
+
+<style lang="sass" scoped>
+    h1
+        font-family: "Arial", sans-serif
+        text-align: center
+        line-height: 100vh
+        color: #FFF
+        text-shadow: 1px 1px 10px #000
+        margin: 0
+        padding: 0
+
+    a
+        text-decoration: none
+        color: inherit
+</style>
